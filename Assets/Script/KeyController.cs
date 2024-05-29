fea96f3d8632ecc2fb5960964fbbee91564c52c2
Palash Choubisa <palash.gamedev@gmail.com>
2024-05-29 13:13:33 +0530

    Simple rotating animations on key is added

--- Assets/Script/KeyController.cs
+++ Assets/Script/KeyController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+   
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
+           
             Destroy(gameObject);
 
-            
         }
         
     }
