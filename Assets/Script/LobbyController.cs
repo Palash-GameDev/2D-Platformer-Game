84fdf20d1b5cb1869407ea9265361cf420e8e848
Palash Choubisa <palash.gamedev@gmail.com>
2024-06-12 00:09:55 +0530

    Instruction panel added

--- Assets/Script/LobbyController.cs
+++ Assets/Script/LobbyController.cs
@@ -9,18 +9,23 @@ public class LobbyController : MonoBehaviour
 {
     public Button playButton;
     public Button quitButton;
+    public Button instruction;
+    public Button backToLobby;
     public GameObject LevelSelection;
-   
+    public GameObject instructionPanel;
+
     // Start is called before the first frame update
     void Awake()
     {
         playButton.onClick.AddListener(PlayGame);
         quitButton.onClick.AddListener(QuitGame);
-       
+        instruction.onClick.AddListener(ShowInstruction);
+        backToLobby.onClick.AddListener(BackToLobby);
+
     }
     void PlayGame()
     {
-       SoundManager.Instance.Play(Sounds.ButtonClick);
+        SoundManager.Instance.Play(Sounds.ButtonClick);
         LevelSelection.SetActive(true);
     }
     void QuitGame()
@@ -28,6 +33,16 @@ public class LobbyController : MonoBehaviour
         SoundManager.Instance.Play(Sounds.ButtonClick);
         Application.Quit();
 
+    }
+
+    void ShowInstruction()
+    {
+        instructionPanel.SetActive(true);
+    }
+
+    void BackToLobby()
+    {
+        instructionPanel.SetActive(false);
     }
 
 
